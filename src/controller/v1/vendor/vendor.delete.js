e17243201e2a342d5df0775fe837e3fd712221f0
DevPatel210 <devhpatel2@gmail.com>
2023-12-21 17:26:05 +0530

    deleted products of a vendor when deleting a vendor

--- src/controller/v1/vendor/vendor.delete.js
+++ src/controller/v1/vendor/vendor.delete.js
@@ -1,7 +1,11 @@
 const { response, resMessage } = require("../../../helpers/common");
 const { Vendor } = require("../../../models/vendor.model");
+const { Product } = require("../../../models/product.model");
 const makeMongoDbService = require("../../../services/mongoDbService")({
   model: Vendor,
+});
+const makeMongoDbServiceProduct = require("../../../services/mongoDbService")({
+  model: Product,
 });
 
 // Delete a Vendor with the specified id in the request
@@ -14,6 +18,13 @@ exports.deleteVendor = async (req) => {
       return response(true, resMessage.vendorNotFound, null);
     }
 
+    const products = await makeMongoDbServiceProduct.getDocumentByQuery({
+      vendor: vendor._id
+    });
+
+    for (let product of products) {
+      await makeMongoDbServiceProduct.softDeleteDocument(product._id);
+    }
     vendor = await makeMongoDbService.softDeleteDocument(id);
     if (vendor) {
       return response(false, resMessage.vendorDeleted, null);
