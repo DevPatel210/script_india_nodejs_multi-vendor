c7a561eae40b0cd67901a23292f14d6d34ac3525
Apple <apple@MacBook-Pro-3.local>
2024-03-12 14:59:00 +0530

    Tracking Details Validaion Changed!

--- src/validations/order.validation.js
+++ src/validations/order.validation.js
@@ -23,10 +23,10 @@ module.exports = {
 	
 	addTrackingDetails: [
 		body("order_id", "Enter valid order id").notEmpty().isString().isMongoId(),
-		body("tracking_number", "Enter valid tracking number").notEmpty().isString(),
-		body("tracking_link", "Enter valid tracking link").notEmpty().isString(),
-		body("remarks", "Please enter remarks").notEmpty().isString(),
-		body("carrier", "Please enter carrier").notEmpty().isString(),
-		body("delivery_date", "Please enter delivery date").notEmpty().isString(),
+		body("tracking_number", "Enter valid tracking number").optional().isString(),
+		body("tracking_link", "Enter valid tracking link").optional().isString(),
+		body("remarks", "Please enter remarks").optional().isString(),
+		body("carrier", "Please enter carrier").optional().isString(),
+		body("delivery_date", "Please enter delivery date").optional().isString(),
 	]
 };
