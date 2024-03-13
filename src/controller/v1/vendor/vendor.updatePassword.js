4c35c7bd865d947e9c9d4444f085d40600bc53c3
Apple <apple@MacBook-Pro-3.local>
2024-03-13 13:28:10 +0530

    All velidation msg pass on single object

--- src/controller/v1/vendor/vendor.updatePassword.js
+++ src/controller/v1/vendor/vendor.updatePassword.js
@@ -6,46 +6,68 @@ const _ = require("lodash");
 const { hashPassword, comparePassword } = require("../../../services/bcryptService");
 const { response, resMessage } = require("../../../helpers/common");
 
+const { validationResult } = require("express-validator");
+
 exports.updatePassword = async (req) => {
-	try {
-		const { oldPassword, newPassword, confirmPassword } = req.body;
-		
-		// Check if newPassword and confirmPassword match
-		if (newPassword !== confirmPassword) {
-			return response(true, "New password and confirm password do not match", null, null, 400);
-		}
-		
-		// Get the vendor's current password from the database
-		const vendor = await makeMongoDbServiceVendor.getSingleDocumentById(req.params.vendorId);
-		if (!vendor) {
-			return response(true, "Vendor not found", null, null, 404);
-		}
-		
-		// Verify old password
-		const isPasswordMatch = await comparePassword(oldPassword, vendor.password);
-		if (!isPasswordMatch) {
-			return response(true, "Old password is incorrect", null, null, 400);
-		}
+    try {
+        const errors = validationResult(req);
 
-		// Hash the new password
-		const hashedNewPassword = await hashPassword(newPassword);
+        // Initialize techMsg object to collect all error messages
+        let techMsg = {};
 
-		// Update password in the database
-		const updatedVendor = await makeMongoDbServiceVendor.updateDocument(req.params.vendorId, { password: hashedNewPassword });
+        // Check if there are validation errors
+        if (!errors.isEmpty()) {
+            // Collect all error messages into techMsg object
+            errors.array().forEach(error => {
+                techMsg[error.param] = error.msg;
+            });
+        }
 
-		return response(
-			false,
-			resMessage.success,
-			null,
-			_.pick(updatedVendor, [
-				"_id",
-				"first_name",
-				"last_name",
-				"email",
-			]),
-			200
-		);
-	} catch (error) {
-		return response(true, null, error.message, error.stack, 500);
-	}
+        // No validation errors, proceed with password update logic
+        const { oldPassword, newPassword, confirmPassword } = req.body;
+        
+        // Check if newPassword and confirmPassword match
+        if (newPassword !== confirmPassword) {
+            techMsg.confirmPassword = "Confirm password does not match new password";
+        }
+        
+        // Get the vendor's current password from the database
+        const vendor = await makeMongoDbServiceVendor.getSingleDocumentById(req.params.vendorId);
+        if (!vendor) {
+            techMsg.vendor = "Vendor not found";
+        }
+        
+        // Verify old password
+        const isPasswordMatch = await comparePassword(oldPassword, vendor.password);
+        if (!isPasswordMatch) {
+            techMsg.oldPassword = "Old password is incorrect";
+        }
+
+        // Check if techMsg object is empty
+        if (Object.keys(techMsg).length > 0) {
+            // Return response with techMsg containing all error messages
+            return response(true, "The request cannot be fulfilled due to bad syntax", null, techMsg, 400);
+        }
+
+        // Hash the new password
+        const hashedNewPassword = await hashPassword(newPassword);
+
+        // Update password in the database
+        const updatedVendor = await makeMongoDbServiceVendor.updateDocument(req.params.vendorId, { password: hashedNewPassword });
+
+        return response(
+            false,
+            resMessage.success,
+            null,
+            _.pick(updatedVendor, [
+                "_id",
+                "first_name",
+                "last_name",
+                "email",
+            ]),
+            200
+        );
+    } catch (error) {
+        return response(true, null, error.message, error.stack, 500);
+    }
 };
