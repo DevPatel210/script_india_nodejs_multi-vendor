896dffe0102c8459293d21d3e03c954e9e26445a
Harshit <apple@MacBook-Pro-3.local>
2024-04-02 16:37:20 +0530

    cart bean issue fixed

--- src/controller/v1/cart/cart.add.js
+++ src/controller/v1/cart/cart.add.js
@@ -1,37 +1,74 @@
 const { Cart } = require("../../../models/cart.model");
 const { response, resMessage } = require("../../../helpers/common");
 
+// exports.add = async (req) => {
+//   try {
+//     const { product_id, quantity, bean } = req.body;
+//     const { _id } = req.user;
+
+//     var result = await Cart.findOne({ user: _id }).then((cart) => {
+//       if (cart) {
+//         const cartItem = cart.cartItems.find(
+//           (item) => item.product.toString() === product_id
+//         );
+//         if (cartItem) {
+//           cartItem.quantity = quantity;
+//           cartItem.bean = bean;
+//           return cart.save();
+//         } else {
+//           cart.cartItems.push({
+//             product: product_id,
+//             quantity: quantity,
+//             bean: bean,
+//           });
+//           return cart.save();
+//         }
+//       } else {
+//         return Cart.create({
+//           user: _id,
+//           cartItems: [{ product: product_id, quantity: quantity, bean: bean }],
+//         });
+//       }
+//     });
+
+//     return response(false, resMessage.success, null, result, 201);
+//   } catch (error) {
+//     throw response(true, null, error.message, error.stack, 500);
+//   }
+// };
 exports.add = async (req) => {
   try {
     const { product_id, quantity, bean } = req.body;
     const { _id } = req.user;
 
-    var result = await Cart.findOne({ user: _id }).then((cart) => {
-      if (cart) {
-        const cartItem = cart.cartItems.find(
-          (item) => item.product.toString() === product_id
-        );
-        if (cartItem) {
-          cartItem.quantity = quantity;
-          cartItem.bean = bean;
-          return cart.save();
-        } else {
-          cart.cartItems.push({
-            product: product_id,
-            quantity: quantity,
-            bean: bean,
-          });
-          return cart.save();
-        }
-      } else {
-        return Cart.create({
-          user: _id,
-          cartItems: [{ product: product_id, quantity: quantity, bean: bean }],
-        });
-      }
-    });
+    // Find the cart for the current user
+    let cart = await Cart.findOne({ user: _id });
 
-    return response(false, resMessage.success, null, result, 201);
+    if (cart) {
+      // Check if the product already exists in the cart
+      const existingCartItem = cart.cartItems.find(
+        (item) => item.product.toString() === product_id && item.bean === bean
+      );
+
+      if (existingCartItem) {
+        // If the product with the same bean already exists, update its quantity
+        existingCartItem.quantity += quantity;
+      } else {
+        // If the product with the same bean doesn't exist, add it to the cart
+        cart.cartItems.push({ product: product_id, quantity, bean });
+      }
+
+      // Save the updated cart
+      cart = await cart.save();
+    } else {
+      // If the cart doesn't exist, create a new cart and add the product to it
+      cart = await Cart.create({
+        user: _id,
+        cartItems: [{ product: product_id, quantity, bean }],
+      });
+    }
+
+    return response(false, resMessage.success, null, cart, 201);
   } catch (error) {
     throw response(true, null, error.message, error.stack, 500);
   }
--- src/controller/v1/cart/cart.get.js
+++ src/controller/v1/cart/cart.get.js
@@ -2,55 +2,62 @@ const { Cart } = require("../../../models/cart.model");
 const { Vendor } = require("../../../models/vendor.model");
 const { Category } = require("../../../models/category.model"); // Import the Category model
 const makeMongoDbService = require("../../../services/mongoDbService")({
-	model: Cart,
+  model: Cart,
 });
 const makeMongoDbServiceVendor = require("../../../services/mongoDbService")({
-	model: Vendor,
+  model: Vendor,
 });
 const { response, resMessage } = require("../../../helpers/common");
 
 exports.get = async (req) => {
-	try {
-		const { _id } = req.user;
-		let vendors = await makeMongoDbServiceVendor.getDocumentByQuery({
-			status: { $ne: 'D'}
-		});
-		vendors = vendors.reduce((obj, item) => (obj[item._id] = item, obj) ,{});
+  try {
+    const { _id } = req.user;
+    let vendors = await makeMongoDbServiceVendor.getDocumentByQuery({
+      status: { $ne: "D" },
+    });
+    vendors = vendors.reduce((obj, item) => ((obj[item._id] = item), obj), {});
 
-		let result = await makeMongoDbService.getSingleDocumentByQueryPopulate(
-			{ user: _id }, null, ["cartItems.product"]
-		);
+    let result = await makeMongoDbService.getSingleDocumentByQueryPopulate(
+      { user: _id },
+      null,
+      ["cartItems.product"]
+    );
 
-		let cartItems = [];
-		for(let product of result.cartItems) {
-			quantity = product._doc.quantity;
-			product = product._doc.product;
-			let vendor = vendors[product.vendor];
-			if (!vendor || vendor.status == "D") {
-				vendor = {commission: 0};
-			}
+    let cartItems = [];
+    for (let product of result.cartItems) {
+      quantity = product._doc.quantity;
+      bean = product._doc.bean;
+      product = product._doc.product;
+      let vendor = vendors[product.vendor];
+      if (!vendor || vendor.status == "D") {
+        vendor = { commission: 0 };
+      }
 
-			// Fetch category details
-			const category = await Category.findById(product.category);
-			const categoryName = category ? category.name : "Unknown"; // Use "Unknown" if category is not found
+      // Fetch category details
+      const category = await Category.findById(product.category);
+      const categoryName = category ? category.name : "Unknown"; // Use "Unknown" if category is not found
 
-			cartItems.push({
-				...product._doc,
-				quantity,
-				commission: ((product.price*quantity*vendor.commission)/100),
-				finalPrice: (product.price + ((product.price*vendor.commission)/100))*quantity,
-				vendor: (!vendor || vendor.status == "D") ? {} : vendor,
-				category: categoryName // Include category name in the cart item
-			});
-		}
-		
-		finalResult = {
-			id: result.id,
-			user: result.user,
-			cartItems: cartItems
-		}
-		return response(false, resMessage.success, null, finalResult,200);
-	} catch (error) {
-		throw response(true, null, error.message, error.stack,500);
-	}
+      cartItems.push({
+        ...product._doc,
+        bean,
+        quantity,
+        commission: (product.price * quantity * vendor.commission) / 100,
+        finalPrice:
+          (product.price + (product.price * vendor.commission) / 100) *
+          quantity,
+        bean,
+        vendor: !vendor || vendor.status == "D" ? {} : vendor,
+        category: categoryName, // Include category name in the cart item
+      });
+    }
+
+    finalResult = {
+      id: result.id,
+      user: result.user,
+      cartItems: cartItems,
+    };
+    return response(false, resMessage.success, null, finalResult, 200);
+  } catch (error) {
+    throw response(true, null, error.message, error.stack, 500);
+  }
 };
