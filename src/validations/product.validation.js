0f2d1cfd50bae1e0b7a880c86b736f15d1c5b958
Harshit <apple@MacBook-Pro-3.local>
2024-04-03 16:44:42 +0530

    samll change

--- src/validations/product.validation.js
+++ src/validations/product.validation.js
@@ -1,66 +1,103 @@
 const { body, check, query } = require("express-validator");
 
 const customValidator = (value) => {
-  if (typeof value === 'string' || Array.isArray(value)) {
+  if (typeof value === "string" || Array.isArray(value)) {
     return true;
   }
   return false;
 };
 
 module.exports = {
-            
   addProduct: [
     body("title", "title can not be empty").notEmpty().isString(),
-    body("subTitle", "subTitle can not be empty").optional().notEmpty().isString(),
+    body("subTitle", "subTitle can not be empty")
+      .optional()
+      .notEmpty()
+      .isString(),
     body("author", "author can not be empty").optional().notEmpty().isString(),
     body("description", "description can not be empty").notEmpty().isString(),
     body("price", "price can not be empty").notEmpty().isNumeric(),
     body("image", "image can not be empty").notEmpty().isArray(),
-    body("category", "category can not be empty").notEmpty().isString(),
-    body("isSoldOut", "isSoldOut can not be empty").optional().default(false).isBoolean(),
+    body("category", "category can not be empty")
+      .notEmpty()
+      .optional()
+      .isString(),
+    body("isSoldOut", "isSoldOut can not be empty")
+      .optional()
+      .default(false)
+      .isBoolean(),
     body("weight", "weight can not be empty").notEmpty().isString(),
     body("origins", "origins can not be empty").notEmpty().isString(),
     //body("missions", "missions can not be empty").notEmpty().isString(),
     body("roast", "roast can not be empty").notEmpty().isString(),
     body("bean", "bean can not be empty").notEmpty(),
     // body("extraAttr", "extraAttr can not be empty").optional().notEmpty().isArray(),
-    check('extraAttr.*.title').if(check('extraAttr').exists()).notEmpty().withMessage('Title is required in extraAttr'),
-    check('extraAttr.*.value').if(check('extraAttr').exists()).notEmpty().withMessage('Value is required in extraAttr'),
+    check("extraAttr.*.title")
+      .if(check("extraAttr").exists())
+      .notEmpty()
+      .withMessage("Title is required in extraAttr"),
+    check("extraAttr.*.value")
+      .if(check("extraAttr").exists())
+      .notEmpty()
+      .withMessage("Value is required in extraAttr"),
   ],
 
   updateProduct: [
     body("product_id", "product_id can not be empty").notEmpty().isString(),
     body("title", "title can not be empty").optional().notEmpty().isString(),
-    body("subTitle", "subTitle can not be empty").optional().notEmpty().isString(),
+    body("subTitle", "subTitle can not be empty")
+      .optional()
+      .notEmpty()
+      .isString(),
     body("author", "author can not be empty").optional().notEmpty().isString(),
-    body("description", "description can not be empty").optional().notEmpty().isString(),
+    body("description", "description can not be empty")
+      .optional()
+      .notEmpty()
+      .isString(),
     body("price", "price can not be empty").optional().notEmpty().isNumeric(),
-    body("image", "image can not be empty").optional().notEmpty().custom(customValidator),
-    body("category", "category can not be empty").optional().notEmpty().isString(),
+    body("image", "image can not be empty")
+      .optional()
+      .notEmpty()
+      .custom(customValidator),
+    body("category", "category can not be empty")
+      .optional()
+      .notEmpty()
+      .isString(),
     body("status", "status can not be empty").optional().notEmpty().isString(),
     body("weight", "weight can not be empty").optional().notEmpty().isString(),
-    body("origins", "origins can not be empty").optional().notEmpty().isString(),
+    body("origins", "origins can not be empty")
+      .optional()
+      .notEmpty()
+      .isString(),
     //body("missions", "missions can not be empty").optional().notEmpty().isString(),
     body("roast", "roast can not be empty").optional().notEmpty().isString(),
     body("bean", "bean can not be empty").optional().notEmpty(),
-    body("isSoldOut", "isSoldOut can not be empty").optional().optional().default(false).isBoolean(),
-    body("extraAttr", "extraAttr can not be empty").optional().notEmpty().isArray()
+    body("isSoldOut", "isSoldOut can not be empty")
+      .optional()
+      .optional()
+      .default(false)
+      .isBoolean(),
+    body("extraAttr", "extraAttr can not be empty")
+      .optional()
+      .notEmpty()
+      .isArray(),
   ],
 
   approveProduct: [
-    body('product_id', "product_id can not be empty").notEmpty().isString(),
+    body("product_id", "product_id can not be empty").notEmpty().isString(),
   ],
-  
+
   getProduct: [
-    query('product_id', "product_id can not be empty").notEmpty().isString(),
+    query("product_id", "product_id can not be empty").notEmpty().isString(),
   ],
 
   getAllProducts: [
-    query("pageNumber", "pageNumber parameter should be number").default(1).toInt(),
+    query("pageNumber", "pageNumber parameter should be number")
+      .default(1)
+      .toInt(),
   ],
 
-  
   deleteProduct: [
-    body('product_id', "product_id can not be empty").notEmpty().isString()
-  ]
-}
+    body("product_id", "product_id can not be empty").notEmpty().isString(),
+  ],
+};
