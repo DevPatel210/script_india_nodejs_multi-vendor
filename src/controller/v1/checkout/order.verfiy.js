9a422fbee78ad23b50bafd16453a304662526fe0
Harshit <apple@MacBook-Pro-3.local>
2024-04-04 16:00:00 +0530

    Payment Verify Flow Fix!!

--- src/controller/v1/checkout/order.verfiy.js
+++ src/controller/v1/checkout/order.verfiy.js
@@ -1,17 +1,16 @@
 const { response } = require("../../../helpers/common");
 const { verifyPayment } = require("../../../services/payment");
-
+const { default: mongoose } = require("mongoose");
 const { Order } = require("../../../models/order.model");
 const { User } = require("../../../models/user.model");
 const { sendEmail } = require("../../../services/email");
 
 const makeMongoDbServiceOrder = require("../../../services/mongoDbService")({
-	model: Order,
+  model: Order,
 });
 const makeMongoDbServiceUser = require("../../../services/mongoDbService")({
-	model: User,
+  model: User,
 });
-
 
 // exports.verifyOrder = async (req) => {
 // 	try {
@@ -42,57 +41,63 @@ const makeMongoDbServiceUser = require("../../../services/mongoDbService")({
 // };
 
 exports.verifyOrder = async (req) => {
-	let order_id = req.body.order_id;
-	let paymentId = req.body.paymentId;
-	try {
-		let order;
+  let order_id = req.body.order_id;
+  let paymentId = req.body.paymentId;
+  try {
+    let order;
 
-		// Check if order_id is provided and is a valid ObjectId
-		if (!order_id || !mongoose.Types.ObjectId.isValid(order_id)) {
-			throw {
-				status: "BAD_REQUEST",
-				message: "The request cannot be fulfilled due to bad syntax",
-				data: {
-					order_id: "Enter valid order id"
-				}
-			};
-		}
+    // Check if order_id is provided and is a valid ObjectId
+    if (!order_id || !mongoose.Types.ObjectId.isValid(order_id)) {
+      throw {
+        status: "BAD_REQUEST",
+        message: "The request cannot be fulfilled due to bad syntax",
+        data: {
+          order_id: "Enter valid order id",
+        },
+      };
+    }
 
-		let paymentIntent = await verifyPayment(paymentId);
-		if (paymentIntent.status === "succeeded") {
-			order = await makeMongoDbServiceOrder.findOneAndUpdateDocument(
-				{ _id: order_id },
-				{ payment_status : "C", status: 'P', paymentId }
-			);
-			const user = await makeMongoDbServiceUser.getDocumentById(order.user_id);
-			const message = getPaymentSuccessfulMessage(order);
-			await sendEmail(user.email,'Payment Successful', message);
-			return response(false, "Payment received successfully.", null, order, 200);
-		} else {
-			order = await makeMongoDbServiceOrder.findOneAndUpdateDocument(
-				{ _id: order_id },
-				{ payment_status: "PF", status: "PF", paymentId } // Update status to "PF" (Payment Failed)
-			);
-			return response(true, "Order payment failed.", null, order, 400);
-		}
-	} catch (error) {
-		// If an error occurs, update the order status to "PF" and throw an error
-		const order = await makeMongoDbServiceOrder.findOneAndUpdateDocument(
-			{ _id: order_id },
-			{ payment_status: "PF", status: "PF", paymentId } // Update status to "PF" (Payment Failed)
-		);
-		throw response(true, null, error.message, error.stack, 500);
-	}
+    let paymentIntent = await verifyPayment(paymentId);
+    if (paymentIntent.status === "succeeded") {
+      order = await makeMongoDbServiceOrder.findOneAndUpdateDocument(
+        { _id: order_id },
+        { payment_status: "C", status: "P", paymentId }
+      );
+      const user = await makeMongoDbServiceUser.getDocumentById(order.user_id);
+      const message = getPaymentSuccessfulMessage(order);
+      await sendEmail(user.email, "Payment Successful", message);
+      return response(
+        false,
+        "Payment received successfully.",
+        null,
+        order,
+        200
+      );
+    } else {
+      order = await makeMongoDbServiceOrder.findOneAndUpdateDocument(
+        { _id: order_id },
+        { payment_status: "PF", status: "PF", paymentId } // Update status to "PF" (Payment Failed)
+      );
+      return response(true, "Order payment failed.", null, order, 400);
+    }
+  } catch (error) {
+    // If an error occurs, update the order status to "PF" and throw an error
+    const order = await makeMongoDbServiceOrder.findOneAndUpdateDocument(
+      { _id: order_id },
+      { payment_status: "PF", status: "PF", paymentId } // Update status to "PF" (Payment Failed)
+    );
+    console.error(error.message);
+    throw response(true, null, error.message, error.stack, 500);
+  }
 };
 
-
-function getPaymentSuccessfulMessage(order){
-	return `
+function getPaymentSuccessfulMessage(order) {
+  return `
 		Dear customer,<br>
 		Your payment is completed successfully. Please find the details of your order below: 
 		<h4>Order id:</h4> ${order._id.toString()}
 		<h4>Shipping Address:</h4> ${order.shippingAddress}
 		<h4>Billing Address:</h4> ${order.billingAddress}
 		<h4>Final Price:</h4> $ ${order.accounting.finalTotal}
-	`
+	`;
 }
