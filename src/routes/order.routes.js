b5dd59bb7d2cb40e7771c08041093469531e3422
Harshit <apple@MacBook-Pro-3.local>
2024-04-04 17:19:07 +0530

    admin make by shipped

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -6,22 +6,65 @@ const validate = require("../validations/handler");
 const rules = require("../validations/order.validation");
 const { isVendor, verifyToken, isAdmin } = require("../middleware/auth.mdl");
 
+router.get(
+  "/",
+  validate(rules.getOrder),
+  verifyToken,
+  isAdmin,
+  isVendor,
+  order.getOrder
+);
 
-router.get("/", validate(rules.getOrder), verifyToken, isAdmin, isVendor, order.getOrder);
+router.get(
+  "/getPaid",
+  isVendor,
+  isAdmin,
+  validate(rules.getOrderPaid),
+  order.getOrderPaid
+);
 
-router.get("/getPaid", isVendor, isAdmin, validate(rules.getOrderPaid), order.getOrderPaid);
+router.get(
+  "/getShipped",
+  isVendor,
+  isAdmin,
+  validate(rules.getOrderShipped),
+  order.getOrderShipped
+);
 
-router.get("/getShipped", isVendor, isAdmin, validate(rules.getOrderShipped), order.getOrderShipped);
+router.get(
+  "/getCancel",
+  isVendor,
+  isAdmin,
+  validate(rules.getOrderCancel),
+  order.getOrderCancel
+);
 
-router.get("/getCancel", isVendor, isAdmin, validate(rules.getOrderCancel), order.getOrderCancel);
+router.get(
+  "/getPaymentFailed",
+  isVendor,
+  isAdmin,
+  validate(rules.getOrderPaymentFailed),
+  order.getOrderPaymentFailed
+);
 
-router.get("/getPaymentFailed", isVendor, isAdmin, validate(rules.getOrderPaymentFailed), order.getOrderPaymentFailed);
-
-router.get("/getByDate", validate(rules.getOrdersByDate), isVendor, isAdmin, order.getOrdersByDate);
+router.get(
+  "/getByDate",
+  validate(rules.getOrdersByDate),
+  isVendor,
+  isAdmin,
+  order.getOrdersByDate
+);
 
 router.post("/update", validate(rules.updateOrder), order.updateOrder);
 
-router.post("/addTrackingDetails", verifyToken, isVendor, validate(rules.addTrackingDetails), order.addTrackingDetails);
+router.post(
+  "/addTrackingDetails",
+  verifyToken,
+  isVendor,
+  isAdmin,
+  validate(rules.addTrackingDetails),
+  order.addTrackingDetails
+);
 
 router.post("/delete", validate(rules.deleteOrder), order.deleteOrder);
 
