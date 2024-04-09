a92dbe5f6867b9f41064c96778b957ff0fde4b5f
VHarshit2213 <vachhaniharshit2213@gmail.com>
2024-04-09 17:34:18 +0530

    get markup_price

--- src/controller/v1/product/product.get.js
+++ src/controller/v1/product/product.get.js
@@ -5,326 +5,378 @@ const { Vendor } = require("../../../models/vendor.model");
 const { Category } = require("../../../models/category.model");
 const Review = require("../../../models/review.model");
 const makeMongoDbService = require("../../../services/mongoDbService")({
-	model: Product,
+  model: Product,
 });
 const makeMongoDbServiceVendor = require("../../../services/mongoDbService")({
-	model: Vendor,
+  model: Vendor,
 });
 const makeMongoDbServiceOrder = require("../../../services/mongoDbService")({
-	model: Order,
+  model: Order,
 });
 const makeMongoDbServiceReview = require("../../../services/mongoDbService")({
-	model: Review,
+  model: Review,
 });
 
 const makeMongoDbServiceCategory = require("../../../services/mongoDbService")({
-	model: Category,
+  model: Category,
 });
 
 // Retrieve and return all products from the database.
 exports.findAll = async (req) => {
-	try {
-		let meta = {};
-		const pageNumber = parseInt(req.query.pageNumber);
-		if (isNaN(pageNumber) || pageNumber < 1) {
-			throw new Error('Invalid pageNumber');
-		}
-		const pageSize = 10;
-		const skip = pageNumber === 1 ? 0 : parseInt((pageNumber - 1) * pageSize);
-		let sortCriteria = { _id: -1 };
-		if(req.query && req.query.sort) {
-			if (req.query.sort=='atoz') {
-				sortCriteria = { title: 1};
-			} else if (req.query.sort=='ztoa') {
-				sortCriteria = {title: -1};
-			} else if (req.query.sort=='priceLow') {
-				sortCriteria = {price: 1};
-			} else if (req.query.sort='priceHigh') {
-				sortCriteria = {price: -1};
-			}
-		}
-		const searchValue = req.query.search;
-		let vendors = await makeMongoDbServiceVendor.getDocumentByQuery({
-			status: { $ne: 'D'}
-		});
-		vendors = vendors.reduce((obj, item) => (obj[item._id] = item, obj) ,{});
+  try {
+    let meta = {};
+    const pageNumber = parseInt(req.query.pageNumber);
+    if (isNaN(pageNumber) || pageNumber < 1) {
+      throw new Error("Invalid pageNumber");
+    }
+    const pageSize = 10;
+    const skip = pageNumber === 1 ? 0 : parseInt((pageNumber - 1) * pageSize);
+    let sortCriteria = { _id: -1 };
+    if (req.query && req.query.sort) {
+      if (req.query.sort == "atoz") {
+        sortCriteria = { title: 1 };
+      } else if (req.query.sort == "ztoa") {
+        sortCriteria = { title: -1 };
+      } else if (req.query.sort == "priceLow") {
+        sortCriteria = { price: 1 };
+      } else if ((req.query.sort = "priceHigh")) {
+        sortCriteria = { price: -1 };
+      }
+    }
+    const searchValue = req.query.search;
+    let vendors = await makeMongoDbServiceVendor.getDocumentByQuery({
+      status: { $ne: "D" },
+    });
+    vendors = vendors.reduce((obj, item) => ((obj[item._id] = item), obj), {});
 
-		let matchCondition = { };
-		if (searchValue && searchValue.trim() !== "") {
-			matchCondition.$and = [
-				{
-					$or: [
-						{ title: { $regex: searchValue, $options: "i" } },
-						{ sub_title: { $regex: searchValue, $options: "i" } },
-						{ author: { $regex: searchValue, $options: "i" } },
-						{ description: { $regex: searchValue, $options: "i" } },
-						{ category: { $regex: searchValue, $options: "i" } },
-					],
-				},
-			];
-		}
-		//filter base on Origins
-		if (req.query.origins) {
-			if (!matchCondition.$and) {
-				matchCondition.$and = [{
-					origins: req.query.origins
-				}]
-			} else {
-				matchCondition.$and.push({
-					origins: req.query.origins
-				});
-			}
-		}
+    let matchCondition = {};
+    if (searchValue && searchValue.trim() !== "") {
+      matchCondition.$and = [
+        {
+          $or: [
+            { title: { $regex: searchValue, $options: "i" } },
+            { sub_title: { $regex: searchValue, $options: "i" } },
+            { author: { $regex: searchValue, $options: "i" } },
+            { description: { $regex: searchValue, $options: "i" } },
+            { category: { $regex: searchValue, $options: "i" } },
+          ],
+        },
+      ];
+    }
+    //filter base on Origins
+    if (req.query.origins) {
+      if (!matchCondition.$and) {
+        matchCondition.$and = [
+          {
+            origins: req.query.origins,
+          },
+        ];
+      } else {
+        matchCondition.$and.push({
+          origins: req.query.origins,
+        });
+      }
+    }
 
-		//filter base on missions
-		if (req.query.missions) {
-			if (!matchCondition.$and) {
-				matchCondition.$and = [{
-					missions: req.query.missions
-				}]
-			} else {
-				matchCondition.$and.push({
-					missions: req.query.missions
-				});
-			}
-		}
+    //filter base on missions
+    if (req.query.missions) {
+      if (!matchCondition.$and) {
+        matchCondition.$and = [
+          {
+            missions: req.query.missions,
+          },
+        ];
+      } else {
+        matchCondition.$and.push({
+          missions: req.query.missions,
+        });
+      }
+    }
 
-		//filter base on roast
-		if (req.query.roast) {
-			if (!matchCondition.$and) {
-				matchCondition.$and = [{
-					roast: req.query.roast
-				}]
-			} else {
-				matchCondition.$and.push({
-					roast: req.query.roast
-				});
-			}
-		}
+    //filter base on roast
+    if (req.query.roast) {
+      if (!matchCondition.$and) {
+        matchCondition.$and = [
+          {
+            roast: req.query.roast,
+          },
+        ];
+      } else {
+        matchCondition.$and.push({
+          roast: req.query.roast,
+        });
+      }
+    }
 
+    if (req.query.filterBy == "A" || req.query.filterBy == "P") {
+      if (!matchCondition.$and) {
+        matchCondition.$and = [
+          {
+            status: req.query.filterBy,
+          },
+        ];
+      } else {
+        matchCondition.$and.push({
+          status: req.query.filterBy,
+        });
+      }
+    }
 
+    let productsList, productCount;
+    if (req.isVendor) {
+      if (!matchCondition.$and) {
+        matchCondition = { vendor: req.vendor._id, status: { $ne: "D" } };
+      } else {
+        matchCondition.$and.push({
+          vendor: req.vendor._id,
+          status: { $ne: "D" },
+        });
+      }
+      productsList = await makeMongoDbService.getDocumentByCustomAggregation([
+        { $match: matchCondition },
+        {
+          $project: {
+            title: 1,
+            subTitle: 1,
+            author: 1,
+            description: 1,
+            price: 1,
+            offer_price: 1,
+            image: 1,
+            category: 1,
+            status: 1,
+            isSoldOut: 1,
+            weight: 1,
+            origins: 1,
+            missions: 1,
+            roast: 1,
+            bean: 1,
+            vendor: 1,
+            markup_price: 1,
+            extraAttr: 1,
+            createdAt: 1,
+            updatedAt: 1,
+          },
+        },
+        { $sort: sortCriteria },
+        // { $skip: skip },
+        // { $limit: pageSize },
+      ]);
+      productCount = await makeMongoDbService.getCountDocumentByQuery(
+        matchCondition
+      );
+    } else {
+      if (!matchCondition.$and) {
+        matchCondition = { status: { $ne: "D" } };
+      } else {
+        matchCondition.$and.push({ status: { $ne: "D" } });
+      }
+      productsList = await makeMongoDbService.getDocumentByCustomAggregation([
+        { $match: matchCondition },
+        {
+          $project: {
+            title: 1,
+            subTitle: 1,
+            author: 1,
+            description: 1,
+            price: 1,
+            offer_price: 1,
+            image: 1,
+            category: 1,
+            status: 1,
+            vendor: 1,
+            isSoldOut: 1,
+            weight: 1,
+            origins: 1,
+            missions: 1,
+            roast: 1,
+            bean: 1,
+            markup_price: 1,
+            extraAttr: 1,
+            createdAt: 1,
+            updatedAt: 1,
+          },
+        },
+        { $sort: sortCriteria },
+        // { $skip: skip },
+        // { $limit: pageSize },
+      ]);
+      productCount = await makeMongoDbService.getCountDocumentByQuery(
+        matchCondition
+      );
+    }
 
+    let reviewedProducts = [];
+    if (req.query && req.query.userId) {
+      const productsSet = new Set();
+      let matchConditionOrder = {
+        $and: [{ status: { $ne: "D" } }, { user_id: req.query.userId }],
+      };
 
-		if (req.query.filterBy=='A' || req.query.filterBy=='P') {
-			if (!matchCondition.$and) {
-				matchCondition.$and = [{
-					status: req.query.filterBy
-				}]
-			} else {
-				matchCondition.$and.push({
-					status: req.query.filterBy
-				});
-			}
-		}
+      const orders = await makeMongoDbServiceOrder.getDocumentByQuery(
+        matchConditionOrder
+      );
 
-		let productsList, productCount;
-		if(req.isVendor){
-			if(!matchCondition.$and){
-				matchCondition = { vendor: req.vendor._id, status: { $ne: "D" } }
-			}else{
-				matchCondition.$and.push({ vendor: req.vendor._id, status: { $ne: "D" } })
-			}
-			productsList =
-				await makeMongoDbService.getDocumentByCustomAggregation([
-					{ $match: matchCondition },
-					{
-						$project: {
-							title: 1,
-							subTitle: 1,
-							author: 1,
-							description: 1,
-							price: 1,
-							offer_price: 1,
-							image: 1,
-							category: 1,
-							status: 1,
-							isSoldOut: 1,
-							weight:1,
-							origins:1,
-							missions:1,
-							roast:1,
-							bean:1,
-							vendor: 1,
-							extraAttr: 1,
-							createdAt:1,
-							updatedAt:1,
-						},
-					},
-					{ $sort: sortCriteria },
-					// { $skip: skip },
-					// { $limit: pageSize },
-				]);
-			productCount = await makeMongoDbService.getCountDocumentByQuery(matchCondition);
-		}else{
-			if(!matchCondition.$and){
-				matchCondition = { status: { $ne: "D" } }
-			}else{
-				matchCondition.$and.push({ status: { $ne: "D" } })
-			}
-			productsList =
-				await makeMongoDbService.getDocumentByCustomAggregation([
-					{ $match: matchCondition },
-					{
-						$project: {
-							title: 1,
-							subTitle: 1,
-							author: 1,
-							description: 1,
-							price: 1,
-							offer_price: 1,
-							image: 1,
-							category: 1,
-							status: 1,
-							vendor: 1,
-							isSoldOut: 1,
-							weight:1,
-							origins:1,
-							missions:1,
-							roast:1,
-							bean:1,
-							extraAttr: 1,
-							createdAt:1,
-							updatedAt:1,
-						},
-					},
-					{ $sort: sortCriteria },
-					// { $skip: skip },
-					// { $limit: pageSize },
-				]);
-			productCount = await makeMongoDbService.getCountDocumentByQuery(matchCondition);
-		}
+      for (let order of orders) {
+        for (let product of order.accounting.cartAccountingList) {
+          productsSet.add(product.productId.toString());
+        }
+      }
 
-		let reviewedProducts = [];
-		if(req.query && req.query.userId){
-			const productsSet = new Set();
-			let matchConditionOrder = { 
-				$and: [
-					{ status: { $ne: 'D' } },
-					{ user_id: req.query.userId }
-				]
-			};
+      reviewedProducts = Array.from(productsSet);
+    }
+    productsList = await Promise.all(
+      productsList.map(async (product) => {
+        let vendor = vendors[product.vendor];
+        if (!vendor || vendor.status == "D") {
+          vendor = { commission: 0 };
+        }
+        let reviews = await makeMongoDbServiceReview.getDocumentByQueryPopulate(
+          {
+            product: product._id.toString(),
+            status: { $ne: "D" },
+          },
+          null,
+          ["user"]
+        );
 
-			const orders = await makeMongoDbServiceOrder.getDocumentByQuery(matchConditionOrder); 
+        let category = await makeMongoDbServiceCategory.getDocumentById(
+          product.category
+        );
 
-			for (let order of orders) {
-				for (let product of order.accounting.cartAccountingList) {
-					productsSet.add(product.productId.toString());
-				}
-			}
+        return {
+          ...product,
+          category,
+          canAddReview: reviewedProducts.includes(product._id.toString()),
+          reviews,
+          vendorDetails:
+            !vendor || vendor.status == "D"
+              ? {}
+              : {
+                  email: vendor.email,
+                  first_name: vendor.first_name,
+                  last_name: vendor.last_name,
+                  commission: vendor.commission,
+                  display: vendor.display,
+                },
+          commission: (product.price * vendor.commission) / 100,
+          finalPrice: product.price + (product.price * vendor.commission) / 100,
+        };
+      })
+    );
 
-			reviewedProducts = Array.from(productsSet);
-		}
-		productsList = await Promise.all(productsList.map(async (product) => {
-			let vendor = vendors[product.vendor];
-			if (!vendor || vendor.status == "D") {
-				vendor = {commission: 0};
-			}
-			let reviews = await makeMongoDbServiceReview.getDocumentByQueryPopulate({
-				product: product._id.toString(),
-				status: { $ne: 'D' }
-			},null,["user"]);
-			
-			let category = await makeMongoDbServiceCategory.getDocumentById(product.category)
+    let displayTrueProducts = productsList.filter(
+      (product) => product.vendorDetails.display
+    );
+    let displayFalseProducts = productsList.filter(
+      (product) => !product.vendorDetails.display
+    );
 
-			return {
-				...product,
-				category,
-				canAddReview: reviewedProducts.includes(product._id.toString()),
-				reviews,
-				vendorDetails: (!vendor || vendor.status == "D") ? {} : {email: vendor.email, first_name: vendor.first_name, last_name: vendor.last_name, commission: vendor.commission, display: vendor.display},
-				commission: ((product.price*vendor.commission)/100),
-				finalPrice: product.price + ((product.price*vendor.commission)/100),
-			}
-		}));
+    productsList = [...displayTrueProducts, ...displayFalseProducts];
 
-		let displayTrueProducts = productsList.filter((product) => product.vendorDetails.display)
-		let displayFalseProducts = productsList.filter((product) => !product.vendorDetails.display)
+    productsList = productsList.slice(
+      (pageNumber - 1) * pageSize,
+      pageNumber * pageSize
+    );
 
-		productsList = [...displayTrueProducts, ...displayFalseProducts];
-
-		productsList = productsList.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
-
-		meta = {
-			pageNumber,
-			pageSize,
-			totalCount: productCount,
-			prevPage: parseInt(pageNumber) === 1 ? false : true,
-			nextPage:
-				parseInt(productCount) / parseInt(pageSize) <= parseInt(pageNumber)
-				? false
-				: true,
-			totalPages: Math.ceil(parseInt(productCount) / parseInt(pageSize)),
-		};
-		return response(false, null, resMessage.success, {
-			result: productsList,
-			meta 
-		},200);
-	} catch (error) {
-		return response(true, null, error.message, error.stack,500);
-	}
+    meta = {
+      pageNumber,
+      pageSize,
+      totalCount: productCount,
+      prevPage: parseInt(pageNumber) === 1 ? false : true,
+      nextPage:
+        parseInt(productCount) / parseInt(pageSize) <= parseInt(pageNumber)
+          ? false
+          : true,
+      totalPages: Math.ceil(parseInt(productCount) / parseInt(pageSize)),
+    };
+    return response(
+      false,
+      null,
+      resMessage.success,
+      {
+        result: productsList,
+        meta,
+      },
+      200
+    );
+  } catch (error) {
+    return response(true, null, error.message, error.stack, 500);
+  }
 };
 
-
-
 exports.findById = async (req) => {
-	try {
-		let isProduct;
-		if (req.isVendor) {
-			isProduct = await makeMongoDbService.getSingleDocumentByQuery({ 
-				vendor: req.vendor._id, _id: req.query.product_id
-			});
-		} else {
-			isProduct = await makeMongoDbService.getSingleDocumentById(
-				req.query.product_id
-			);
-		}
-		if (!isProduct || isProduct.status == "D") {
-			return response(true, null, resMessage.notFound,[],404);
-		}
-		const vendor = await makeMongoDbServiceVendor.getDocumentById(isProduct.vendor);
-		if (!vendor || vendor.status == "D") {
-			return response(true, null, resMessage.vendorNotFound,[],404);
-		}
-		
-		let reviewedProducts = [];
-		if(req.query && req.query.userId){
-			const productsSet = new Set();
-			let matchConditionOrder = { 
-				$and: [
-					{ status: { $ne: 'D' } },
-					{ user_id: req.query.userId }
-				]
-			};
+  try {
+    let isProduct;
+    if (req.isVendor) {
+      isProduct = await makeMongoDbService.getSingleDocumentByQuery({
+        vendor: req.vendor._id,
+        _id: req.query.product_id,
+      });
+    } else {
+      isProduct = await makeMongoDbService.getSingleDocumentById(
+        req.query.product_id
+      );
+    }
+    if (!isProduct || isProduct.status == "D") {
+      return response(true, null, resMessage.notFound, [], 404);
+    }
+    const vendor = await makeMongoDbServiceVendor.getDocumentById(
+      isProduct.vendor
+    );
+    if (!vendor || vendor.status == "D") {
+      return response(true, null, resMessage.vendorNotFound, [], 404);
+    }
 
-			const orders = await makeMongoDbServiceOrder.getDocumentByQuery(matchConditionOrder); 
+    let reviewedProducts = [];
+    if (req.query && req.query.userId) {
+      const productsSet = new Set();
+      let matchConditionOrder = {
+        $and: [{ status: { $ne: "D" } }, { user_id: req.query.userId }],
+      };
 
-			for (let order of orders) {
-				for (let product of order.accounting.cartAccountingList) {
-					productsSet.add(product.productId.toString());
-				}
-			}
+      const orders = await makeMongoDbServiceOrder.getDocumentByQuery(
+        matchConditionOrder
+      );
 
-			reviewedProducts = Array.from(productsSet);
-		}
+      for (let order of orders) {
+        for (let product of order.accounting.cartAccountingList) {
+          productsSet.add(product.productId.toString());
+        }
+      }
 
-		let category = await makeMongoDbServiceCategory.getDocumentById(isProduct.category)
+      reviewedProducts = Array.from(productsSet);
+    }
 
-		let reviews = await makeMongoDbServiceReview.getDocumentByQueryPopulate({
-			product: isProduct._id.toString(),
-			status: { $ne: 'D' }
-		},null,["user"]);
+    let category = await makeMongoDbServiceCategory.getDocumentById(
+      isProduct.category
+    );
 
-		isProduct = {
-			...isProduct._doc,
-			reviews,
-			category: category ? category: isProduct.category,
-			canAddReview: reviewedProducts.includes(isProduct._id.toString()),
-			vendorDetails: {email: vendor.email, first_name: vendor.first_name, last_name: vendor.last_name, commission: vendor.commission},
-			commission: ((isProduct.price*vendor.commission)/100),
-			finalPrice: isProduct.price + ((isProduct.price*vendor.commission)/100),
-		}
-		return response(false, null, resMessage.success, isProduct,200);
-	} catch (error) {
-		return response(true, null, error.message, error.stack,500);
-	}
+    let reviews = await makeMongoDbServiceReview.getDocumentByQueryPopulate(
+      {
+        product: isProduct._id.toString(),
+        status: { $ne: "D" },
+      },
+      null,
+      ["user"]
+    );
+
+    isProduct = {
+      ...isProduct._doc,
+      reviews,
+      category: category ? category : isProduct.category,
+      canAddReview: reviewedProducts.includes(isProduct._id.toString()),
+      vendorDetails: {
+        email: vendor.email,
+        first_name: vendor.first_name,
+        last_name: vendor.last_name,
+        commission: vendor.commission,
+      },
+      commission: (isProduct.price * vendor.commission) / 100,
+      finalPrice: isProduct.price + (isProduct.price * vendor.commission) / 100,
+    };
+    return response(false, null, resMessage.success, isProduct, 200);
+  } catch (error) {
+    return response(true, null, error.message, error.stack, 500);
+  }
 };
