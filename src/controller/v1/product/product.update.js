982c68056da262aa149f267ba8ad55ae55606dd5
VHarshit2213 <vachhaniharshit2213@gmail.com>
2024-04-09 14:26:41 +0530

    markup_price

--- src/controller/v1/product/product.update.js
+++ src/controller/v1/product/product.update.js
@@ -13,28 +13,28 @@ const { sendEmail } = require("../../../services/email");
 exports.Update = async (req) => {
   try {
     let isProduct;
-    if(req.isVendor){
-      isProduct = await makeMongoDbService.getSingleDocumentByQuery({ 
-				vendor: req.vendor._id, _id: req.body.product_id
-			});
-      req.body.status = 'P'
-    }else{
+    if (req.isVendor) {
+      isProduct = await makeMongoDbService.getSingleDocumentByQuery({
+        vendor: req.vendor._id,
+        _id: req.body.product_id,
+      });
+      req.body.status = "P";
+    } else {
       isProduct = await makeMongoDbService.getSingleDocumentById(
         req.body.product_id
       );
     }
 
     if (!isProduct) {
-      return response(true, resMessage.notFound, null,[],404);
+      return response(true, resMessage.notFound, null, [], 404);
     }
     const productData = req.body; // update product payload
 
-    if (typeof productData.bean === 'string') {
-      productData.bean = productData.bean.split(',');
+    if (typeof productData.bean === "string") {
+      productData.bean = productData.bean.split(",");
     }
 
-    
-    if(typeof productData.image == 'string'){
+    if (typeof productData.image == "string") {
       delete productData.image;
     }
     const updatedProduct = await makeMongoDbService.findOneAndUpdateDocument(
@@ -42,84 +42,100 @@ exports.Update = async (req) => {
       productData
     );
 
-    if(req.isVendor){
-      const productDetails = await makeMongoDbService.getSingleDocumentById(req.body.product_id);
-      const vendorDetails = await makeMongoDbServiceVendor.getSingleDocumentById(isProduct.vendor);
+    if (req.isVendor) {
+      const productDetails = await makeMongoDbService.getSingleDocumentById(
+        req.body.product_id
+      );
+      const vendorDetails =
+        await makeMongoDbServiceVendor.getSingleDocumentById(isProduct.vendor);
       const message = getPendingApprovalMessage(productDetails, vendorDetails);
-		  await sendEmail('','Update Product Approval Pending', message);
+      await sendEmail("", "Update Product Approval Pending", message);
     }
 
-    return response(false, resMessage.success, null, updatedProduct,200);
+    return response(false, resMessage.success, null, updatedProduct, 200);
   } catch (error) {
-    return response(true, null, error.message, error.stack,500);
+    return response(true, null, error.message, error.stack, 500);
   }
 };
 
 exports.approveProduct = async (req) => {
   try {
-    if(!req.isAdmin){
-      return response(true, null, resMessage.failed,[],403);
+    if (!req.isAdmin) {
+      return response(true, null, resMessage.failed, [], 403);
     }
     let isProduct = await makeMongoDbService.getSingleDocumentById(
       req.body.product_id
     );
 
-    if (!isProduct || isProduct.status=='D') {
-      return response(true, resMessage.notFound, null,[],404);
-    }
-    
-    if (isProduct.status=='A') {
-      return response(true, 'Product already approved', null,[],400);
+    if (!isProduct || isProduct.status == "D") {
+      return response(true, resMessage.notFound, null, [], 404);
     }
 
-    isProduct.status='A';
+    if (isProduct.status == "A") {
+      return response(true, "Product already approved", null, [], 400);
+    }
+
+    isProduct.status = "A";
+    // Check if markup_price is provided in the request
+    if (req.body.markup_price !== undefined) {
+      // If provided, add the markup_price field to the product object
+      isProduct.markup_price = req.body.markup_price;
+    }
+
     const updatedProduct = await makeMongoDbService.findOneAndUpdateDocument(
       { _id: req.body.product_id },
       isProduct
     );
 
-    const vendorDetails = await makeMongoDbServiceVendor.getSingleDocumentById(isProduct.vendor);
+    const vendorDetails = await makeMongoDbServiceVendor.getSingleDocumentById(
+      isProduct.vendor
+    );
     const message = getApprovedMessage(isProduct);
-		await sendEmail(vendorDetails.email,'Product Approval Notification', message, true);
+    await sendEmail(
+      vendorDetails.email,
+      "Product Approval Notification",
+      message,
+      true
+    );
 
-    return response(false, resMessage.success, null, updatedProduct,200);
+    return response(false, resMessage.success, null, updatedProduct, 200);
   } catch (error) {
     console.log(error);
-    return response(true, null, error.message, error.stack,500);
+    return response(true, null, error.message, error.stack, 500);
   }
-}
+};
 
 exports.unApproveProduct = async (req) => {
   try {
-    if(!req.isAdmin){
-      return response(true, null, resMessage.failed,[],403);
+    if (!req.isAdmin) {
+      return response(true, null, resMessage.failed, [], 403);
     }
     let isProduct = await makeMongoDbService.getSingleDocumentById(
       req.body.product_id
     );
 
-    if (!isProduct || isProduct.status=='D') {
-      return response(true, resMessage.notFound, null,[],404);
-    }
-    
-    if (isProduct.status=='P') {
-      return response(true, 'Product already in pending state', null,[],400);
+    if (!isProduct || isProduct.status == "D") {
+      return response(true, resMessage.notFound, null, [], 404);
     }
 
-    isProduct.status='P';
+    if (isProduct.status == "P") {
+      return response(true, "Product already in pending state", null, [], 400);
+    }
+
+    isProduct.status = "P";
     const updatedProduct = await makeMongoDbService.findOneAndUpdateDocument(
       { _id: req.body.product_id },
       isProduct
     );
 
-    return response(false, resMessage.success, null, updatedProduct,200);
+    return response(false, resMessage.success, null, updatedProduct, 200);
   } catch (error) {
-    return response(true, null, error.message, error.stack,500);
+    return response(true, null, error.message, error.stack, 500);
   }
-}
+};
 
-function getPendingApprovalMessage(product, vendor){
-	return `
+function getPendingApprovalMessage(product, vendor) {
+  return `
 		A product has been updated by a vendor and is currently pending your approval.
     <br><br>
     Product Details: <br>
@@ -129,16 +145,16 @@ function getPendingApprovalMessage(product, vendor){
     - Price: $ ${product.price} <br>
     <br><br>
     Please review the product and take necessary action.
-	`
+	`;
 }
 
-function getApprovedMessage(product){
-	return `
+function getApprovedMessage(product) {
+  return `
 		We are pleased to inform you that your product has been approved by the admin and is now live on our platform.
     <br><br>
     Product Details: <br>
     - Product Name: ${product.title} <br>
     - Description: ${product.description} <br>
     - Price: $ ${product.price} <br>
-	`
+	`;
 }
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,86 +2,90 @@ const { array } = require("joi");
 const mongoose = require("mongoose");
 
 const extraAttrSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	value: {
-		type: String,
-		required: true,
-	},
+  title: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
 });
 
-const productSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	subTitle: {
-		type: String
-		
-	},
-	author: {
-		type: String
-		
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	// offer_price: {
-	// 	type: Number,
-	// 	required: true,
-	// },
-	image: {
-		type: Array,
-		required: true,
-	},
-	// category: {
-	// 	type: String,
-	// 	required: false,
-	// },
-	status: {
-		type: String,
-		required: true,
-		default: "P",
-	},
-	isSoldOut: {
-		type: Boolean,
-		required: true,
-		default: false,
-	},
-	weight:{
-		type: String,
-		required: true,
-	},
-	origins:{
-		type: String,
-		required: true,
-	},
-	missions:{
-		type: String,
-		required: true,
-	},
-	roast:{
-		type: String,
-		required: true,
-	},
-	bean:{
-		type: [String],
-		required: true,
-	},
-	vendor: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Vendor',
-		required: true,
-	},
-	extraAttr: [extraAttrSchema],
-},{timestamps: true});
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+    },
+    author: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    // offer_price: {
+    // 	type: Number,
+    // 	required: true,
+    // },
+    markup_price: {
+      type: Number,
+    },
+    image: {
+      type: Array,
+      required: true,
+    },
+    // category: {
+    // 	type: String,
+    // 	required: false,
+    // },
+    status: {
+      type: String,
+      required: true,
+      default: "P",
+    },
+    isSoldOut: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    weight: {
+      type: String,
+      required: true,
+    },
+    origins: {
+      type: String,
+      required: true,
+    },
+    missions: {
+      type: String,
+      required: true,
+    },
+    roast: {
+      type: String,
+      required: true,
+    },
+    bean: {
+      type: [String],
+      required: true,
+    },
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+    extraAttr: [extraAttrSchema],
+  },
+  { timestamps: true }
+);
 
 exports.Product = mongoose.model("Product", productSchema);
 exports.productSchema = productSchema;
