a1a0f7c2b73cdba925ba8ac883c46f0857d08dda
Harshit Vachhani <vachhaniharshit2213@gmail.com>
2024-05-09 03:38:19 +0530

    Mail id change on feedback Function

--- src/controller/v1/feedback/feedback.add.js
+++ src/controller/v1/feedback/feedback.add.js
@@ -6,18 +6,18 @@ const { sendEmail } = require("../../../services/email");
 const { response, resMessage } = require("../../../helpers/common");
 
 exports.add = async (req) => {
-	try {
-		const feedback = await makeMongoDbService.createDocument(req.body);
-		const message = getFeedbackMessage(req.body);
-    await sendEmail("", "New message received", message);
-		return response(false, resMessage.success, null, feedback,201);
-	} catch (error) {
-		console.log(error);
-		throw response(true, null, error.message, error.stack,500);
-	}
+  try {
+    const feedback = await makeMongoDbService.createDocument(req.body);
+    const message = getFeedbackMessage(req.body);
+    await sendEmail("drjacks@obu.edu", "New message received", message); // Updated email address
+    return response(false, resMessage.success, null, feedback, 201);
+  } catch (error) {
+    console.log(error);
+    throw response(true, null, error.message, error.stack, 500);
+  }
 };
 
-function getFeedbackMessage({name, email, message}) {
+function getFeedbackMessage({ name, email, message }) {
   return `
 		A new message is submitted. Please find the details below:
     <br><br>
